339e8fd45e20f756022de8cac134b421f34f6643
jrso15 <janilleolegario@gmail.com>
2022-06-25 02:22:49 +0800

    added evironment to config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
 module.exports = {
   reactStrictMode: true,
-}
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: "",
+    loader: "imgix",
+    path: "",
+  },
+  exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  },
+  env: {
+    SITE_URL: "http://localhost:3000/",
+    GITHUB_CLIENT_ID: "Iv1.3679576c64587634",
+    GITHUB_CLIENT_SECRET: "24d8bc6e063c34fe3068b913fcae61a39696cccf",
+    API_URL: "https://api.openweathermap.org/",
+    API_KEY: "5da766f475dc01ceda16cb940c87a9e4",
+  },
+};
