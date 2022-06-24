e2926bfc353ae950290bfa0c1f52f5bb63ac39f5
jrso15 <janilleolegario@gmail.com>
2022-06-25 05:41:56 +0800

    footer component and styles

---
+++ components/Footer.js
@@ -0,0 +1,7 @@
+import styles from "../styles/Home.module.scss";
+
+const Footer = () => {
+    return (<footer className={styles.footer}><p>© 2022 Weather Forecast</p></footer>);
+};
+
+export default Footer;
