d934ebb6a38b7b5628bbd73b76e91d9508335a72
jrso15 <janilleolegario@gmail.com>
2022-06-25 10:42:12 +0800

    remove keys

--- next.config.js
+++ next.config.js
@@ -17,8 +17,8 @@ module.exports = {
   env: {
     SITE_URL: "http://localhost:3000/",
     GITHUB_CLIENT_ID: "Iv1.3679576c64587634",
-    GITHUB_CLIENT_SECRET: "24d8bc6e063c34fe3068b913fcae61a39696cccf",
+    GITHUB_CLIENT_SECRET: "<GITHUB-SECRET>",
     API_URL: "https://api.openweathermap.org/",
-    API_KEY: "5da766f475dc01ceda16cb940c87a9e4",
+    API_KEY: "<API-KEY>",
   },
 };
